6287fd683891c6e833dfc0c7d0c8ce3947ae89c6
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-08-03 19:06:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 //your JS code here. If required.
+function updateClock(){
+	const timeElement = document.getElementById('timer');
+	const currentDate = new Date();
+
+	const hours = currentDate.getHours().toString().padStart(2, '0');
+	const minutes = currentDate.getMinutes().toString().padStart(2, '0');
+	const seconds = currentDate.getSeconds().toString().padStart(2, '0');
+	const formattedTime = `${hours}:${minutes}:${seconds}`;
+
+	const daysOfWeek = ['Sunday', 'Monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+	const dayOfWeek = daysOfWeek.[currentDate.getDay()];
+	const month = currentDate.toLocaleString('default', { month: 'long'});
+	const dayInMonth = currentDate.getDate();
+	const year = currentDate.getFullYear();
+	const formattedDate = `${dayOfWeek}, ${month} ${dayInMonth}, ${year}`;
+
+	timeElement.innerHtml = `${formattedDate} ${formattedTime}`;
+}
+
+setInterval(updateClock, 1000);
+
+updateClock();
