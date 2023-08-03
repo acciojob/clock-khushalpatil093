7e1c5c3fb7670c1c12c45facdf6372f36b77f637
Khushal Patil <58685911+khushalpatil093@users.noreply.github.com>
2023-08-03 19:34:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,23 +1,13 @@
 //your JS code here. If required.
-function updateClock(){
-	const timeElement = document.getElementById('timer');
-	const currentDate = new Date();
+function updateDateTime() {
+  const datetimeElement = document.getElementById('timer');
+  const currentDate = new Date();
 
-	const hours = currentDate.getHours().toString().padStart(2, '0');
-	const minutes = currentDate.getMinutes().toString().padStart(2, '0');
-	const seconds = currentDate.getSeconds().toString().padStart(2, '0');
-	const formattedTime = `${hours}:${minutes}:${seconds}`;
+  const dateTime = currentDate.toLocaleString();
 
-	const daysOfWeek = ['Sunday', 'Monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
-	const dayOfWeek = daysOfWeek.[currentDate.getDay()];
-	const month = currentDate.toLocaleString('default', { month: 'long'});
-	const dayInMonth = currentDate.getDate();
-	const year = currentDate.getFullYear();
-	const formattedDate = `${month}/${dayInMonth}/${year},`;
-
-	timeElement.innerHtml = `${formattedDate} ${formattedTime}`;
+  datetimeElement.innerHTML = `${dateTime}`;
 }
 
-setInterval(updateClock, 1000);
+setInterval(updateDateTime, 1000);
 
-updateClock();
+updateDateTime();
